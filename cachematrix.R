910843518a5fbca97833d165312f71be16ef3517
EricChen90 <eric.suryawirawan@gmail.com>
2015-06-21 15:58:59 +0700

    cacheMatrix functions
    cacheMatrix function for R programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function (makeCacheMatrix) save the matrix into cache
+## The second (cacheSolve) will compute the inverse of the matrix that
+## has been saved by makeCacheMatrix, and save the result to the cache,
+## if the same matrix that has been computed by cacheSolve passed into the function
+## the cacheSolve will get the cached data instead of computing
+## the inverse of the matrix using solve function again which can saves time
+## if working using a large matrix repeatedly.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) m <<- solve
+      getinv <- function() m
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The second function cacheSolve
+## computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cacheSolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinv(m)
+      m
 }
+
+## Checking the function:
+## 1. create the invertible matrix
+## 2. pass the matrix into makeCacheMatrix and assign it to another values
+## 3. pass the value created in step 2 into cacheSolve function
+##    cacheSolve will compute and print the inverse of the matrix
+## 4. using the same value created in step 2 to cacheSolve function will 
+##    give you "getting cached data" message and print the inverse of the matrix
